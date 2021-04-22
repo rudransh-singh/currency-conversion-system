10dce6a08380a201b0371772a7bf582d9fda4380
rudransh-singh <rudranshpratap.singh@students.iiit.ac.in>
2021-04-23 03:42:17 +0530

    first commit(graph.h and README.md)
    added graph.h and README.md files

---
+++ graph.h
@@ -0,0 +1,14 @@
+#ifndef GRAPH__H
+#define GRAPH__H
+typedef struct tradebank tradebank;//typdefing structures
+struct tradebank{
+    char NameOfTradeBank[100];//Stores the name of the trade bank
+    // add graph details later
+};
+typedef struct currency currency;
+struct currency{
+    char NameOfCurrency[100];// stores the name of the currency
+    //add currency value later
+};
+//add graph details later
+#endif // !GRAPH__H
