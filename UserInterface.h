198c1b376cb801e5eb56b24e8d0f45baf13f3b75
Anish Mathur <anishmathur001@gmail.com>
2021-05-06 14:24:25 +0530

    Userinterface.h modified

--- UserInterface.h
+++ UserInterface.h
@@ -11,22 +11,31 @@
 4. The best path of converting from currency A to B along with it's cost and the chosen Trade Bank.You can only choose a single Trade Bank for all the exchanges regarding this query.
 */
 
+PtrToCurrencyNode CreateEmptyCurrList(); //creates a dummy node and returns it's value
+int searchforcurrency(char *inputcurrency, PtrToCurrencyNode C); //search for the input currency in the given currency linked list
+void addcurrency(char *inputcurrency, int vertexid, PtrToCurrencyNode c); //add the input currency to the currency linked list
+void deletecurrency(char *inputcurrency, PtrToCurrencyNode c); //deletes the node from the given currency list
+void printcurrencylist(PtrToCurrencyNode C); //prints currency list on the screen
+void DeleteCurrList(PtrToCurrencyNode C); //frees pointers in the currency list
+void AddTradeBank(char *BankName); //adds a new Tradebank to the linked list
+void AddCurrencyExchange(char *BankName, char *Currency1, char *Currency2, int ConversionRate); //adds an edge in the currency graph of a Tradebank
+void RemoveCurrencyExchange(char* BankName, char* source, char* dest); //deletes an edge in the currency graph of a Tradebank
 
 
 
-void AddTradeBank(char* BankName);// Parth Anish
+// void AddTradeBank(char* BankName);// Parth Anish
 
-void AddCurrencyExchange(char* BankName,char* Currency1 , char* Currency2 , int ConversionRate); // Parth nd Anish
+// void AddCurrencyExchange(char* BankName,char* Currency1 , char* Currency2 , int ConversionRate); // Parth nd Anish
 
-void DeleteTradeBank(char* BankName);//Rohan 
+// void DeleteTradeBank(char* BankName);//Rohan 
 
-void DeleteCurrency(char* BankName,char* CurrencyName); // RP
+// void DeleteCurrency(char* BankName,char* CurrencyName); // RP
  
-// void UpdateCommission(char* BankName,char* Currency1 , char* Curreny2, int commission); // Vikram
-void RemoveCurrencyExchange(char* BankName, char* source, char* dest);
+// // void UpdateCommission(char* BankName,char* Currency1 , char* Curreny2, int commission); // Vikram
+// void RemoveCurrencyExchange(char* BankName, char* source, char* dest);
 
-void UpdateCurrencyExchange(char* BankName, char* Currency1, char* Currency2, int ConversionRate); // Vikram
-void AddCurrencyToGlobalList(char* inputcurrency);
-int LookupCurrencyVertex(char* inputcurrency);
-void PrintAllCurrencies();
+// void UpdateCurrencyExchange(char* BankName, char* Currency1, char* Currency2, int ConversionRate); // Vikram
+// void AddCurrencyToGlobalList(char* inputcurrency);
+// int LookupCurrencyVertex(char* inputcurrency);
+// void PrintAllCurrencies();
 #endif
