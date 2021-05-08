9e29418ec56c7fa23c6c2e4c6cc269f647c1b36b
EswaraRohan <eswara.rohan@students.iiit.ac.in>
2021-05-08 19:07:12 +0530

    Modified main.c

--- main.c
+++ main.c
@@ -1,126 +1,250 @@
-#include "graph.h"
-#include "priorityqueue.h"
 #include <stdio.h>
 #include <stdlib.h>
+//#include "UserInterface.c"
+//#include "graph.c"
+#include "graph.h"
+#include "priorityqueue.h"
 #include "UserInterface.h"
+//#include "priorityqueue.c"
 #include <string.h>
-int main()
-{
-    // while (1)
-    // {
-    //     printf("Enter Bank Name ->");
-    //     char BankName[50];
-    //     scanf("%s", BankName);
-    //     if (strcmp(BankName, "quit") == 0)
-    //         break;
-    //     AddTradeBank(BankName);
-    // }
-    // while (Head != NULL)
-    // {
-    //     printf("%s ", Head->NameOfTradeBank); //dont move head
-    //     Head = Head->next;
-    // }
-    char string1[50] = "SBI";
-    char string2[50] = "JPMC";
-    char string3[50] = "BOB";
-    char string4[50] = "HDFC";
+#include <stdbool.h>
+#include <ctype.h> 
+#include <limits.h>
 
-    AddTradeBank(string1);
-    AddTradeBank("HDFC");
-   AddCurrencyToTradeBank("SBI","rupee");
-//    AddCurrencyToTradeBank("SBi","rupee");
-   AddCurrencyToTradeBank("SBI","dollar");
-   AddCurrencyToTradeBank("SBI","yen");
-    // AddTradeBank(string2);
-    // AddTradeBank(string3);
-    // AddTradeBank(string4);
-     AddCurrencyExchange1("SBI","rupee","dollar",5);
-     AddCurrencyExchange1("SBI","dollar","yen",10);
-     AddCurrencyExchange1("SBI","rupee","yen",7);
-     AddCurrencyToTradeBank("HDFC","rupee");
-     AddCurrencyToTradeBank("HDFC","yen");
-     AddCurrencyExchange1("HDFC","rupee","yen",4);
-    // AddCurrencyExchange("SBI","dollar","dirham",5);
-    // AddCurrencyExchange("SBI","dirham","rupee",5);
-    // AddCurrencyExchange("SBI","rupee","euros",5);
-    // AddCurrencyExchange("SBI","euros","dollar",5);
-    // AddCurrencyExchange("SBI","rupee","yen",5);
-    // RemoveTradeBank("SBI");
-    // RemoveTradeBank("HDFC");
-    // RemoveCurrencyExchange("SBI","rupee","yen");
-    PrintTradeBankList2();
-    DijkstraBankInfo solution;
-    solution=DijkstraOnBankList("rupee","yen");
-    printf("%d %s",solution.mincost, solution.TradeBankName);
-    // printf("\n---------------\n");
-    // RemoveTradeBank(string2);
-    // PrintTradeBankList2();
-    //     printf("\n---------------\n");
+void UserData(){
+    printf("1.Add Trade Bank\n");
+    printf("2.Remove Trade Bank\n"); 
+    printf("3.Add Currency Exchange\n");              //****
+    printf("4.Remove Currency Exchange\n");           //****
+    printf("5.Add Currency To TradeBank\n");
+    printf("6.Search For Currency\n");
+    printf("7.Add Currency\n");
+    printf("8.Delete Curency\n");
+    printf("9.Print Currency List\n");
+    printf("10.Delete Currency List\n");
+    printf("11.Print Trade Bank List\n");             //****
+    printf("12.Print Trade Bank Graph\n");            //****
+    printf("13.Dijkstra On Bank List\n");             //****
+    printf("14.Show UserData\n");
+    printf("15.Exit\n");
+}
 
-    // RemoveTradeBank(string1);
 
-    // PrintTradeBankList2();
-    //    printf("\n---------------\n");
 
-    // RemoveTradeBank(string4);
-
-    // PrintTradeBankList2();
-
-    //checking whether Graphlist works
-    // PtrToGraphList G=NULL;
-    // G=CreateGraph(10);
-    // InsertEdge(G,1,2,12);
-    // InsertEdge(G,1,3,10);
-    // InsertEdge(G,1,4,10);
-    // InsertEdge(G,1,5,11);
-    // InsertEdge(G,1,6,10);
-    // InsertEdge(G,1,7,13);
-    // InsertEdge(G,1,8,12);
-    // InsertEdge(G,1,8,10);
-    // InsertEdge(G,2,8,10);
-    // InsertEdge(G,4,7,10);
-    // InsertEdge(G,3,8,10);
-    // InsertEdge(G,3,6,10);
-
-    // PrintGraph(G);
-
-    //Checking whether Heap works
-    /*
-    PtrToMinHeap H=NULL;
-    H=CreateHeap(11); //CreateMinHeap of size 11
-    InsertMinHeapKeyValue(H,1,3);
-    PrintHeap(H);
-
-    InsertMinHeapKeyValue(H,1,2);
-    PrintHeap(H);
-
-    InsertMinHeapKeyValue(H,1,1);
-    PrintHeap(H);
-
-     DeleteKeyHeap(H,1);
-    PrintHeap(H);
-
-    InsertMinHeapKeyValue(H,1,15);
-    PrintHeap(H);
-
-    InsertMinHeapKeyValue(H,1,5);
-    PrintHeap(H);
-
-    InsertMinHeapKeyValue(H,1,4);
-    PrintHeap(H);
-
-    InsertMinHeapKeyValue(H,1,45);
-    PrintHeap(H);
-
+int main(){
     
-    HeapNode t;
-    DecreaseKey(H,4,1);
-    PrintHeap(H);
+    PtrToCurrencyNode CurrNode;
+    CurrNode = CreateEmptyCurrList();
+    
+    
+    UserData(); 
+    
+    while(1){
+        printf("Enter The Respective Number Of The Required Operation:");
+        char Operation[50];scanf("%s",Operation);
+        
+        
+        if(strcmp(Operation,"1")==0){
+            char TradeBank[50];
+            printf("Enter Name Of The Trade Bank:");
+            scanf("%s",TradeBank);
+            
+            AddTradeBank(TradeBank);
+            printf("\nEnter 14 for Operations Data\n\n");
+        }
+        
+        
+        
+        
+        else if(strcmp(Operation,"2")==0){
+            char TradeBank[50];
+            printf("Enter Name Of The Trade Bank:");
+            scanf("%s",TradeBank);
+            
+            RemoveTradeBank(TradeBank);
+            printf("\nEnter 14 To View Operations Data\n\n");
+    
+        }
+        
+        
+        else if(strcmp(Operation,"3")==0){
+            char TradeBank[50];
+            char Currency1[50];
+            char Currency2[50];
+            int ConversionRate,Commission;
+            
+            printf("Enter Name Of The Trade Bank:");
+            scanf("%s",TradeBank);
+            
+            printf("Enter Currency1:");
+            scanf("%s",Currency1);
+            
+            printf("Enter Currency2:");
+            scanf("%s",Currency2);
+            
+            printf("Enter Conversion Rate:");
+            scanf("%d",&ConversionRate);
+             
+            
+            AddCurrencyExchange1(TradeBank,Currency1,Currency2,ConversionRate);
+            
+            printf("\nEnter 14 To View Operations Data\n\n");
+        }
+        
+        
+        
+        
+        else if(strcmp(Operation,"4")==0){
+            char TradeBank[50];
+            char Currency1[50];
+            char Currency2[50];
+            
+            printf("Enter Name Of The Trade Bank:");
+            scanf("%s",TradeBank);
+            
+            printf("Enter The Source:");
+            scanf("%s",Currency1);
+            
+            printf("Enter The Destiantion:");
+            scanf("%s",Currency2);
+            
+            RemoveCurrencyExchange(TradeBank,Currency1,Currency2);
+            
+            printf("\nEnter 14 To View Operations Data\n\n");
+        }
+        
+        
+        
+        else if(strcmp(Operation,"5")==0){
+            char TradeBank[50];
+            char InputCurrency[50];
+            
+            printf("Enter Name Of The Trade Bank:");
+            scanf("%s",TradeBank);
+            
+            printf("Enter Currency:");
+            scanf("%s",InputCurrency);
+       
+            
+            AddCurrencyToTradeBank(TradeBank,InputCurrency);
+            
+            
+            printf("\nEnter 14 To View Operations Data\n\n");
+            
+        }
+        
+        
+        
+        else if(strcmp(Operation,"6")==0){
+            char Currency[50];
+            
+            printf("Enter Currency1:");
+            scanf("%s",Currency); 
+            
+            searchforcurrency(Currency,CurrNode);
+            
+            printf("\nEnter 14 To View Operations Data\n\n");
+        }
+        
+        
+        
+        else if(strcmp(Operation,"7")==0){
+            char Currency[50];
+            int VertexId;
+            
+            printf("Enter Currency:");
+            scanf("%s",Currency);
+            
+            printf("Enter VertexId:");
+            scanf("%d",&VertexId);
+            
+            addcurrency(Currency,VertexId,CurrNode);
+            
+            printf("\nEnter 14 To View Operations Data\n\n");
+            
+        }
+        
+        
+        else if(strcmp(Operation,"8")==0){
+            char Currency[50];
+            
+            printf("Enter The Currency:");
+            scanf("%s",Currency);
+            
+            deletecurrency(Currency,CurrNode);
+            
+            printf("\nEnter 14 To View Operations Data\n\n");
+        }
+        
+        
+        
+        else if(strcmp(Operation,"9")==0){
+            printcurrencylist(CurrNode);
+            
+            printf("\nEnter 14 for Operations Data\n\n");
+        }
+        
+        
+        
+        else if(strcmp(Operation,"10")==0){
+            DeleteCurrList(CurrNode);
+            
+            printf("\nEnter 14 for Operations Data\n\n");
+        }
+        
+        
+        
+        else if(strcmp(Operation,"11")==0){
+            PrintTradeBankList2();
+            
+            printf("\nEnter 14 for Operations Data\n\n");
+        }
+        
+        
+        
+        else if(strcmp(Operation,"12")==0){
+            //PrintTradeBankGraph();
+            
+            printf("\nEnter 14 for Operations Data\n\n");
+        }
+        
+        
+        
+        else if(strcmp(Operation,"13")==0){
+            char sourcecurrency[50];
+            char destcurrency[50];
+            
+            printf("Enter Source Currency:");
+            scanf("%s",sourcecurrency);
+            
+            printf("Enter DEstination Currency:");
+            scanf("%s",destcurrency);
+            
+            DijkstraOnBankList(sourcecurrency,destcurrency);
+            
+            printf("\nEnter 14 for Operations Data\n\n");
+        }
+        
+        
+        else if(strcmp(Operation,"14")==0){
+            UserData();
+        }
+        
+        
+        else if(strcmp(Operation,"15")==0){
+            break;
+        }
+        
+        else{
+            printf("\nEnter A Valid Input\n");
+            printf("\n");
+            UserData();
+            printf("\nEnter A Valid Input\n");
 
-    t=ExtractMin(H);
-    PrintHeap(H);
+        }
+        
+    }    
+    
+    return 0;
 
-    printf("min-keyvalue pair for H is \n Key=%d and value= %d \n",t.key,t.value);
-    printf("min-keyvalue pair for H is \n Key=%d and value= %d \n",GetMin(H).key,GetMin(H).value);
-*/
 }
